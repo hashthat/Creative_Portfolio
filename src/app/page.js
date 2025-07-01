04478a6b7c9db8c6ea9e467864c623287ca90bb2
hashthat <samalberts73@gmail.com>
2025-07-01 12:24:36 -0500

    modify

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,26 @@
 import Image from "next/image";
 import cosmic from "../../public/background/CosmicJellyfish.png";
-// import RenderModel from "./components/Models/Wizard";
-import dynamic from 'next/dynamic';
 import Navigation from "./components/navigation";
 
-// Dynamically import the RenderModel component to avoid SSR issues
-// const RenderModel = dynamic(() => import('./components/RenderModel'), {
-//  ssr: false,
-// });
 // This is the main page of the Next.js application.
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between relative">
-      <Image src= {cosmic} alt="Cosmic"
-      sizes="100vw"
-      priority
-      fill
-      className="-z-50 w-full object-cover object-center opacity-65"/>
+      <Image
+        src={cosmic}
+        alt="Cosmic"
+        sizes="100vw"
+        priority
+        fill
+        className="-z-50 w-full object-cover object-center opacity-65"
+      />
 
-    <Navigation /> 
-    
-    <div className="w-full h-screen">
-      {<RenderModel />}
-      
+      <Navigation />
+
       <footer className="absolute bottom-0 left-0 right-0 p-4 text-center">
-        <p className="text-sm">© 2023 My Portfolio. All rights reserved.</p>
+        <p className="text-sm">© 2025 My Portfolio. All rights reserved.</p>
       </footer>
-    </div>
     </main>
   );
 }
+
